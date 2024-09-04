27a73099f112caa7eb67d59a7327c40812db8861
pratik123456789123 <pratikkumarmnnit@gmail.com>
2024-09-04 09:09:34 +0530

    Initial changes

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
 import { StyledEngineProvider } from '@mui/material/styles';
-import InputSlider from './InputSlider';
-import InvestmentCalculator from './main';
+import InputSlider from '../InputSlider';
+import InvestmentCalculator from '../main';
 
 ReactDOM.createRoot(document.querySelector('#root')!).render(
   <React.StrictMode>
